94c44d9722fb5316f86f19908bc2191fc3beb26b
Darren Jackson <darren.c.jackson@gmail.com>
2025-05-11 21:57:22 +0100

    Added fugative

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -25,6 +25,7 @@ require('lazy').setup({
   require 'kickstart/plugins/treesitter',
   require 'kickstart/plugins/harpoon',
   require 'kickstart/plugins/noice',
+  require 'kickstart/plugins/fugative',
   --
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
